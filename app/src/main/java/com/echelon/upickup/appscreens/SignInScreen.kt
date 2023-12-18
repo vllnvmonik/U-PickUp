b0595fe026506cc0beb2134291431095d4aab75f
Monica Villanueva <vllnv.monica@gmail.com>
2023-12-18 20:43:48 +0800

    added the textfield components to the ui

--- app/src/main/java/com/echelon/upickup/appscreens/SignInScreen.kt
+++ app/src/main/java/com/echelon/upickup/appscreens/SignInScreen.kt
@@ -8,10 +8,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.echelon.upickup.R
+import com.echelon.upickup.components.EditText
 import com.echelon.upickup.components.LogoImage
 import com.echelon.upickup.components.TitleText
 
@@ -19,7 +21,7 @@ import com.echelon.upickup.components.TitleText
 fun SignInScreen(){
     Surface (modifier = Modifier
         .fillMaxSize(),
-        color = Color.White
+        color = colorResource(id = R.color.background_color)
     ){
         Column (modifier = Modifier,
             horizontalAlignment = Alignment.CenterHorizontally
@@ -27,6 +29,13 @@ fun SignInScreen(){
             Spacer(modifier = Modifier.height(100.dp))
             LogoImage()
             TitleText(text = stringResource(R.string.hey_there))
+            Spacer(modifier = Modifier.height(50.dp))
+            EditText(title = stringResource(id = R.string.e_mail), KeyboardType.Text)
+            Spacer(modifier = Modifier.height(5.dp))
+            EditText(title = stringResource(R.string.password), KeyboardType.Text)
+            Spacer(modifier = Modifier.height(5.dp))
+            EditText(stringResource(R.string.id_number), KeyboardType.Number)
+
         }
     }
 }
