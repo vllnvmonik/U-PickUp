d4eecc57cd1372147d1b9136dfcada01b8de7acb
Monica Villanueva <vllnv.monica@gmail.com>
2023-12-18 20:42:12 +0800

    added textfield composable

--- app/src/main/java/com/echelon/upickup/components/UIComponents.kt
+++ app/src/main/java/com/echelon/upickup/components/UIComponents.kt
@@ -3,13 +3,22 @@ package com.echelon.upickup.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.echelon.upickup.R
@@ -43,3 +52,48 @@ fun TitleText(
         )
     )
 }
+
+@Composable
+fun BasicText(
+    text: String
+) {
+    Text(
+        text = text,
+        style = TextStyle(
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Normal,
+            color = colorResource(id = R.color.edit_text_gray)
+        )
+    )
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditText(
+    title: String,
+    keyboardType: KeyboardType
+) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    TextField(
+        colors = TextFieldDefaults.textFieldColors(
+            unfocusedLabelColor = colorResource(id = R.color.edit_text_gray),
+            focusedLabelColor = colorResource(id = R.color.edit_text_gray),
+            containerColor = colorResource(id = R.color.background_color)
+        ),
+        modifier = Modifier
+            .width(300.dp)
+            .height(60.dp),
+        value = text, onValueChange = { newText ->
+        text = newText
+        },
+        label = {
+            Text(title)
+        },
+        placeholder = {
+            Text(title)
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType)
+    )
+}
+
