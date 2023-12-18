1d3167d7b5d75486e8c42da32d6042cc0ae67391
Monica Villanueva <vllnv.monica@gmail.com>
2023-12-18 20:43:29 +0800

    changed app background color

--- app/src/main/java/com/echelon/upickup/app/UPickUp.kt
+++ app/src/main/java/com/echelon/upickup/app/UPickUp.kt
@@ -4,14 +4,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.echelon.upickup.R
 import com.echelon.upickup.appscreens.SignInScreen
 
 @Composable
 fun UPickUp(){
     Surface (modifier = Modifier
         .fillMaxSize(),
-        color = Color.White
+        color = colorResource(id = R.color.background_color)
     ){
         SignInScreen()
     }
