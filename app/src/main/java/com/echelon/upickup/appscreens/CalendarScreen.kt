de299a84ed1d086aa12ffcbc1dd2f49abc28d2c0
Monica Villanueva <vllnv.monica@gmail.com>
2023-12-29 17:36:21 +0800

    added app screens

---
+++ app/src/main/java/com/echelon/upickup/appscreens/CalendarScreen.kt
@@ -0,0 +1,20 @@
+package com.echelon.upickup.appscreens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.navigation.NavController
+import com.echelon.upickup.R
+import com.echelon.upickup.components.BasicText
+
+@Composable
+fun CalendarScreen(navController: NavController) {
+    Surface (modifier = Modifier
+        .fillMaxSize(),
+        color = colorResource(id = R.color.background_color)
+    ){
+        BasicText(text = "Calendar")
+    }
+}
