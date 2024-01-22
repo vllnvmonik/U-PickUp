df04cb7865994d50ad26bba2496afd0f0f9ec372
Monica Villanueva <vllnv.monica@gmail.com>
2024-01-22 12:05:02 +0800

    added nested navigation routes for auth and app

--- app/src/main/java/com/echelon/upickup/navigation/Router.kt
+++ app/src/main/java/com/echelon/upickup/navigation/Router.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navigation
 import com.echelon.upickup.R
 import com.echelon.upickup.appscreens.CalendarScreen
 import com.echelon.upickup.appscreens.ChatScreen
@@ -19,6 +20,9 @@ sealed class Screen (val route: String) {
     object CalendarScreen: Screen("calendar")
     object ChatScreen: Screen("chat")
     object ProfileScreen: Screen("profile")
+    object AuthRoute: Screen("auth")
+    object AppRoute: Screen("app")
+
 }
 
 sealed class BottomNavItem(
@@ -36,40 +40,31 @@ fun NavController(navController: NavHostController) {
 
     NavHost(
         navController = navController,
-        startDestination = Screen.DashboardScreen.route
+        startDestination = Screen.AuthRoute.route
     ) {
-        composable(Screen.SignUpScreen.route) {
-            SignUpScreen(navController = navController)
+        // application navigation route before logging in
+        navigation(startDestination = Screen.SignInScreen.route, route = Screen.AuthRoute.route){
+            composable(Screen.SignUpScreen.route) {
+                SignUpScreen(navController = navController)
+            }
+            composable(Screen.SignInScreen.route) {
+                SignInScreen(navController = navController)
+            }
         }
-        composable(Screen.SignInScreen.route) {
-            SignInScreen(navController = navController)
-        }
-        composable(Screen.DashboardScreen.route) {
-            DashboardScreen(navController = navController)
-        }
-        composable(Screen.CalendarScreen.route) {
-            CalendarScreen(navController = navController)
-        }
-        composable(Screen.ChatScreen.route) {
-            ChatScreen(navController = navController)
-        }
-        composable(Screen.ProfileScreen.route) {
-            ProfileScreen(navController = navController)
+        // application navigation route after successful log in
+        navigation(startDestination = Screen.DashboardScreen.route, route = Screen.AppRoute.route){
+            composable(Screen.DashboardScreen.route) {
+                DashboardScreen(navController = navController)
+            }
+            composable(Screen.CalendarScreen.route) {
+                CalendarScreen(navController = navController)
+            }
+            composable(Screen.ChatScreen.route) {
+                ChatScreen(navController = navController)
+            }
+            composable(Screen.ProfileScreen.route) {
+                ProfileScreen(navController = navController)
+            }
         }
     }
 }
-
-//@Composable
-//fun ShowBottomNavBar(navController: NavHostController) {
-//    val currentRoute = navController.currentBackStackEntry?.destination?.route
-//    Log.d("sdfsdfsdf", "riute $currentRoute")
-//
-//    if (currentRoute in listOf(
-//            Screen.DashboardScreen.route,
-//            Screen.CalendarScreen.route,
-//            Screen.ChatScreen.route,
-//            Screen.ProfileScreen.route
-//        )) {
-//        BottomNavigationBar(navController)
-//    }
-//}
