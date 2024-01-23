bee424aaa247e11d8bc10fe424fc61c7ee3bf612
Monica Villanueva <vllnv.monica@gmail.com>
2024-01-23 15:31:14 +0800

    implemented composables to the calendar screen

--- app/src/main/java/com/echelon/upickup/appscreens/CalendarScreen.kt
+++ app/src/main/java/com/echelon/upickup/appscreens/CalendarScreen.kt
@@ -1,15 +1,21 @@
 package com.echelon.upickup.appscreens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.echelon.upickup.R
-import com.echelon.upickup.components.TitleText
+import com.echelon.upickup.components.CalendarAnnouncementBox
+import com.echelon.upickup.components.CalendarBox
+import com.echelon.upickup.components.CustomImage
 
 @Composable
 fun CalendarScreen(navController: NavHostController) {
@@ -18,10 +24,19 @@ fun CalendarScreen(navController: NavHostController) {
         color = colorResource(id = R.color.background_color)
     ){
         Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center)
+            modifier = Modifier.fillMaxSize())
         {
-            TitleText(text = "Calendar Content")
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                Arrangement.Center,
+                Alignment.CenterHorizontally
+            ){
+                CustomImage(100,100, R.drawable.logo)
+                CalendarBox()
+                CalendarAnnouncementBox()
+            }
         }
     }
 }
