fec5c642a7f39ef95d08a53251a05c32f0ecf89e
Monica Villanueva <vllnv.monica@gmail.com>
2024-01-23 15:28:55 +0800

    added spacer to the dashboard ui

--- app/src/main/java/com/echelon/upickup/appscreens/DashboardScreen.kt
+++ app/src/main/java/com/echelon/upickup/appscreens/DashboardScreen.kt
@@ -2,12 +2,15 @@ package com.echelon.upickup.appscreens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.echelon.upickup.R
 import com.echelon.upickup.components.CustomImage
@@ -28,6 +31,7 @@ fun DashboardScreen(navController: NavHostController) {
                     .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ){
+                Spacer(modifier = Modifier.height(10.dp))
                 CustomImage(100,100, R.drawable.logo)
                 FeedBox()
             }
