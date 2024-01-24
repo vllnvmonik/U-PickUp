12f1a7f5ffb37ef2d2ebc6e2d9651982ccf33262
Monica Villanueva <vllnv.monica@gmail.com>
2024-01-24 18:33:24 +0800

    implemented composable to the profile screen

--- app/src/main/java/com/echelon/upickup/appscreens/ProfileScreen.kt
+++ app/src/main/java/com/echelon/upickup/appscreens/ProfileScreen.kt
@@ -1,15 +1,24 @@
 package com.echelon.upickup.appscreens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.echelon.upickup.R
-import com.echelon.upickup.components.TitleText
+import com.echelon.upickup.components.ClassDetailsBox
+import com.echelon.upickup.components.CustomColorTitleText
+import com.echelon.upickup.components.LogoutButton
 
 @Composable
 fun ProfileScreen(navController: NavHostController) {
@@ -21,7 +30,22 @@ fun ProfileScreen(navController: NavHostController) {
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center)
         {
-            TitleText(text = "Profile Content")
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                Arrangement.Center,
+                Alignment.CenterHorizontally
+            ){
+                CustomColorTitleText(text = stringResource(R.string.profile_name), color = R.color.dark_green, 22)
+                CustomColorTitleText(text = stringResource(R.string.profile_program), color = R.color.dark_green, 16)
+                Spacer(modifier = Modifier.height(20.dp))
+                ClassDetailsBox()
+                Spacer(modifier = Modifier.height(30.dp))
+                LogoutButton(text = stringResource(R.string.logout)) {
+                    
+                }
+            }
         }
     }
 }
