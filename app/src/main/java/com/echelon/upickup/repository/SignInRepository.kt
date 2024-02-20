cce1e42e69d8a3d4b36afb31136424eec080524e
Monica Villanueva <vllnv.monica@gmail.com>
2024-02-20 11:27:45 +0800

    updated to get the response details of the logged in user

--- app/src/main/java/com/echelon/upickup/repository/SignInRepository.kt
+++ app/src/main/java/com/echelon/upickup/repository/SignInRepository.kt
@@ -4,13 +4,13 @@ import com.echelon.upickup.model.SignIn
 import com.echelon.upickup.network.RetrofitInstance
 import com.echelon.upickup.network.apimodel.SignInRequest
 import com.echelon.upickup.network.apimodel.SignInResponse
+import retrofit2.Response
 
 class SignInRepository {
     private val signInApiService = RetrofitInstance.signInApiService
 
-    suspend fun signIn(credentials: SignIn): SignInResponse? {
+    suspend fun signIn(credentials: SignIn): Response<SignInResponse> {
         val signInRequest = SignInRequest(credentials.idNumber, credentials.password)
-        val response = signInApiService.signIn(signInRequest)
-        return if (response.isSuccessful) response.body() else null
+        return signInApiService.signIn(signInRequest)
     }
 }
