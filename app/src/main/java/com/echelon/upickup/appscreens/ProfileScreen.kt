427c6c3f62b5c166d5f187eb0fd0e250f2f63016
Monica Villanueva <vllnv.monica@gmail.com>
2024-02-20 14:57:18 +0800

    modified profile screen

--- app/src/main/java/com/echelon/upickup/appscreens/ProfileScreen.kt
+++ app/src/main/java/com/echelon/upickup/appscreens/ProfileScreen.kt
@@ -1,5 +1,6 @@
 package com.echelon.upickup.appscreens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,8 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
@@ -23,9 +26,13 @@ import com.echelon.upickup.components.ClassDetailsBox
 import com.echelon.upickup.components.CustomColorTitleText
 import com.echelon.upickup.components.LogoutButton
 import com.echelon.upickup.components.StaticProfile
+import com.echelon.upickup.viewmodel.ProfileViewModel
 
 @Composable
-fun ProfileScreen(navController: NavHostController) {
+fun ProfileScreen(navController: NavHostController, viewModel: ProfileViewModel) {
+    val studentDetails by viewModel.studentDetails.collectAsState()
+    Log.d("ProfileScreen", "Student details: $studentDetails") // Add this line for debugging
+
     Surface (modifier = Modifier
         .fillMaxSize()
         .verticalScroll(rememberScrollState()), // for scrollable screen
@@ -46,13 +53,24 @@ fun ProfileScreen(navController: NavHostController) {
 //                ProfileImage()
                 StaticProfile()
                 Spacer(modifier = Modifier.height(20.dp))
-                CustomColorTitleText(text = stringResource(R.string.profile_name), color = R.color.dark_green, 22, fontWeight = FontWeight.Medium)
-                CustomColorTitleText(text = stringResource(R.string.profile_program), color = R.color.dark_green, 16, FontWeight.Medium)
+                studentDetails?.let { CustomColorTitleText(text = it.first_name, color = R.color.dark_green, 22, fontWeight = FontWeight.Medium) }
+                studentDetails?.let { CustomColorTitleText(text = it.program, color = R.color.dark_green, 16, FontWeight.Medium) }
                 Spacer(modifier = Modifier.height(20.dp))
-                ClassDetailsBox()
+                studentDetails.let { details ->
+                    if (details != null) {
+                        ClassDetailsBox(
+                            email = details.email_ad,
+                            studentId = details.student_id,
+                            gender = details.gender,
+                            age = details.age,
+                            program = details.program,
+                            department = details.department,
+                        )
+                    }
+                }
                 Spacer(modifier = Modifier.height(20.dp))
                 LogoutButton(text = stringResource(R.string.logout)) {
-                    
+
                 }
             }
         }
