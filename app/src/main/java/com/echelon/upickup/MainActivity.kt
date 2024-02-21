758784274aa18d40706b34149a803647d41e19b9
Monica Villanueva <vllnv.monica@gmail.com>
2024-02-21 11:17:11 +0800

    initialized sharedprefs

--- app/src/main/java/com/echelon/upickup/MainActivity.kt
+++ app/src/main/java/com/echelon/upickup/MainActivity.kt
@@ -5,10 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.echelon.upickup.app.UPickUp
+import com.echelon.upickup.sharedprefs.StudentDetailsManager
+import com.echelon.upickup.sharedprefs.TokenManager
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        TokenManager.initialize(this)
+        StudentDetailsManager.initialize(this)
         installSplashScreen()
         setContent {
             UPickUp()
