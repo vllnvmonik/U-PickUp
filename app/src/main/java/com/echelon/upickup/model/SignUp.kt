b819e1f7452d7435b1c82d3d65698d2ea98ffa9b
Monica Villanueva <vllnv.monica@gmail.com>
2024-02-22 20:51:03 +0800

    added sign up dataclass

--- app/src/main/java/com/echelon/upickup/model/SignUp.kt
+++ app/src/main/java/com/echelon/upickup/model/SignUp.kt
@@ -2,6 +2,13 @@ package com.echelon.upickup.model
 
 data class SignUp(
     val name: String,
+    val middleName: String,
+    val lastName: String,
+    val program: String,
+    val department: String,
+    val gender: String,
+    val age: String,
+    val email:String,
     val idNumber: String,
     val password: String,
     val confirmPassword: String
