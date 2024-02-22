8638b0a1438422b9af502838168cb5b634d09428
Monica Villanueva <vllnv.monica@gmail.com>
2024-02-22 20:51:14 +0800

    added sign up ui state

--- app/src/main/java/com/echelon/upickup/uistate/SignUpUIState.kt
+++ app/src/main/java/com/echelon/upickup/uistate/SignUpUIState.kt
@@ -2,6 +2,13 @@ package com.echelon.upickup.uistate
 
 data class SignUpUIState(
     val name: String = "",
+    val middleName: String = "",
+    val lastName: String = "",
+    val gender: String = "",
+    val age: String = "",
+    val program: String = "",
+    val department: String = "",
+    val email: String = "",
     val idNumber: String = "",
     val password: String = "",
     val confirmPassword: String = "",
