ecca239b5baee5d00ece9f299067f78334b72da7
Monica Villanueva <vllnv.monica@gmail.com>
2024-02-28 16:48:13 +0800

    initialized inventory sharedprefs

--- app/src/main/java/com/echelon/upickup/MainActivity.kt
+++ app/src/main/java/com/echelon/upickup/MainActivity.kt
@@ -6,8 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.echelon.upickup.app.UPickUp
 import com.echelon.upickup.sharedprefs.AuthManager
+import com.echelon.upickup.sharedprefs.BooksManager
+import com.echelon.upickup.sharedprefs.ModulesManager
 import com.echelon.upickup.sharedprefs.StudentDetailsManager
 import com.echelon.upickup.sharedprefs.TokenManager
+import com.echelon.upickup.sharedprefs.UniformsManager
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,9 @@ class MainActivity : ComponentActivity() {
         TokenManager.initialize(this)
         StudentDetailsManager.initialize(this)
         AuthManager.initialize(this)
+        UniformsManager.initialize(this)
+        ModulesManager.initialize(this)
+        BooksManager.initialize(this)
         installSplashScreen()
         setContent {
             UPickUp()
